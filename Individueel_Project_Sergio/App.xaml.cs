1c52a4154cf0927b20df1c44f458568dfcae53fd
Sergio Corrente <sergio.corrente@gmail.com>
2020-10-17 03:38:27 -0700

    Add project files.

---
+++ Individueel_Project_Sergio/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Individueel_Project_Sergio
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Individueel_Project_Sergio/Registratie.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Individueel_Project_Sergio
+{
+    /// <summary>
+    /// Interaction logic for Registratie.xaml
+    /// </summary>
+    public partial class Registratie : Window
+    {
+        public Registratie()
+        {
+            InitializeComponent();
+            cmbGebruikerType.Items.Add("Beheerder");
+            cmbGebruikerType.Items.Add("Verkoper");
+            cmbGebruikerType.Items.Add("Magazinier");
+        }
+
+        private void btnToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            int roleID = 0;
+            string hash = "Etnerr0c";
+            string gebruiker = cmbGebruikerType.Text.ToString();
+            switch (gebruiker)
+            {
+                case "Beheerder": roleID = 1; break;
+                case "Verkoper": roleID = 2; break;
+                case "Magazinier": roleID = 3; break;
+
+                default:
+                    break;
+            }
+            if (txtPasswoord.Password == txtConfPasswoord.Password && roleID != 0)
+            {
+                byte[] data = UTF8Encoding.UTF8.GetBytes(txtPasswoord.Password);
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                    using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 }) 
+                    {
+                        ICryptoTransform transform = tripDes.CreateEncryptor();
+                        Byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                        txtPasswoord.Password = Convert.ToBase64String(results, 0, results.Length);
+                    }
+                }
+                using (MagazijnEntities ctx = new MagazijnEntities())
+                {
+                    ctx.PersoneelsIDs.Add(new PersoneelsID()
+                    {
+                        Voornaam = txtVoorNaam.Text,
+                        Achternaam = txtAchterNaam.Text,
+                        Username = txtGebruikeNaam.Text,
+                        RoleID = roleID,
+                        Wachtwoord = txtPasswoord.Password
+
+                    });
+                    ctx.SaveChanges();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Er is iets miss probeer opnieuw");
+            }
+        }
+    }
+}
